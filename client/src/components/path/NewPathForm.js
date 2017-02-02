edeea82acd90a09414018a3561cfa28e052d303a
unknown <anrodriguez@Tic-pc-150.dgcp.local>
2017-02-02 09:07:48 -0400

    accept merge

--- client/src/components/path/NewPathForm.js
+++ client/src/components/path/NewPathForm.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {
+  connect
+} from 'react-redux';
 import R from 'ramda';
 import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaGroup from '../common/TextAreaGroup';
 import RequestButton from '../common/RequestButton';
 import PathItem from './PathItem';
-import { kebabCase } from '../../utils/functions';
-import { validateNewPath } from '../../validators/pathValidator.js'
+import {
+  kebabCase
+} from '../../utils/functions';
+import {
+  validateNewPath
+} from '../../validators/pathValidator.js'
 import withHandlers from '../../utils/withHandlers';
-import { createPath } from '../../actions';
-import { addFlashMessage } from '../../actions/flashMessages';
+import {
+  createPath
+} from '../../actions';
+import {
+  addFlashMessage
+} from '../../actions/flashMessages';
 import './NewPathForm.css';
 
 const url = R.compose(kebabCase, R.replace(/[^a-zA-Z0-9-\s_]/g, ''));
@@ -19,11 +29,9 @@ const initialStaet = {
     name: '',
     url: '',
     description: '',
-    items: [
-      {
-        name: '',
-      },
-    ],
+    items: [{
+      name: '',
+    }, ],
   },
   errors: {},
   changeUrl: true,
@@ -45,28 +53,41 @@ export class NewPathForm extends React.Component {
     this.handleChange = props.handleChange.bind(this);
   }
   handleNameChange(e) {
-    const form = { ...this.state.form, name: e.target.value };
-    if(this.state.changeUrl) {
+    const form = { ...this.state.form,
+      name: e.target.value
+    };
+    if (this.state.changeUrl) {
       form.url = url(e.target.value);
     }
-    this.setState({ form });
+    this.setState({
+      form
+    });
   }
   handleUrlChagne(e) {
     this.setState({
       changeUrl: false,
-      form: { ...this.state.form, url: url(e.target.value) }
+      form: { ...this.state.form,
+        url: url(e.target.value)
+      }
     });
   }
   handleAddItem() {
     const form = this.state.form;
 
     this.setState({
-      form: { ...form, items: [ ...form.items, { name: '' } ] },
+      form: { ...form,
+        items: [...form.items, {
+          name: ''
+        }]
+      },
     });
   }
   handleRemoveItem(indexItem) {
     return (e) => {
-      const { form, errors } = this.state;
+      const {
+        form,
+        errors
+      } = this.state;
       const items = form.items.filter(
         // eslint-disable-next-line
         (item, index) => index != indexItem
@@ -76,18 +97,27 @@ export class NewPathForm extends React.Component {
         // eslint-disable-next-line
         errors.items.filter((item, index) => index != indexItem);
 
-      this.setState({ form: { ...form, items }, errors: newErrors });
+      this.setState({
+        form: { ...form,
+          items
+        },
+        errors: newErrors
+      });
     }
   }
   handleItemsChange(e) {
     const form = this.state.form;
     const items = form.items.map(
       // eslint-disable-next-line
-      (item, index) => index == e.target.dataset.index ?
-        { ...item, name: e.target.value } :
-        item
+      (item, index) => index == e.target.dataset.index ? { ...item,
+        name: e.target.value
+      } : item
     );
-    this.setState({ form: { ...form,  items } });
+    this.setState({
+      form: { ...form,
+        items
+      }
+    });
   }
   handleSubmit(e) {
     e.preventDefault();
@@ -114,71 +144,111 @@ export class NewPathForm extends React.Component {
     );
   }
   render() {
-    const { form, errors } = this.state;
-    return (
-      <form className="NewPathForm-form" onSubmit={ this.handleSubmit }>
-        <TextFieldGroup
-          name="name"
-          placeholder="Name"
-          value={ form.name }
-          errors={ errors.name }
-          onChange={ this.handleNameChange }
-        />
+    const {
+      form,
+      errors
+    } = this.state;
+    return ( <
+      form className = "NewPathForm-form"
+      onSubmit = {
+        this.handleSubmit
+      } >
+      <
+      TextFieldGroup name = "name"
+      placeholder = "Name"
+      value = {
+        form.name
+      }
+      errors = {
+        errors.name
+      }
+      onChange = {
+        this.handleNameChange
+      }
+      />
 
-        <TextFieldGroup
-          name="url"
-          placeholder="Url"
-          value={ form.url }
-          errors={ errors.url }
-          onChange={ this.handleUrlChagne }
-        />
+      <
+      TextFieldGroup name = "url"
+      placeholder = "Url"
+      value = {
+        form.url
+      }
+      errors = {
+        errors.url
+      }
+      onChange = {
+        this.handleUrlChagne
+      }
+      />
 
-        <TextAreaGroup
-          name="description"
-          placeholder="Description"
-          value={ form.description }
-          errors={ errors.description }
-          onChange={ this.handleChange }
-        />
+      <
+      TextAreaGroup name = "description"
+      placeholder = "Description"
+      value = {
+        form.description
+      }
+      errors = {
+        errors.description
+      }
+      onChange = {
+        this.handleChange
+      }
+      />
 
-        <fieldset className="form-group text-left NewPathForm-items"
-          onChange={ this.handleItemsChange }
-        >
-          <h5 className="text-center">Items</h5>
-          {
-            this.state.form.items.map(
-              (item, index) => <PathItem
-                key={ index }
-                index={ index }
-                value={item.name}
-                onDelete={ this.handleRemoveItem(index) }
-                placeholder="Name"
-                errors={ errors.items && errors.items[index] &&
-                  errors.items[index].name }
-              />
-            )
+      <
+      fieldset className = "form-group text-left NewPathForm-items"
+      onChange = {
+        this.handleItemsChange
+      } >
+      <
+      h5 className = "text-center" > Items < /h5> {
+        this.state.form.items.map(
+          (item, index) =>
+          <
+          PathItem key = {
+            index
           }
-          <button
-            type="button"
-            className="btn btn-secondary btn-sm"
-            onClick={ this.handleAddItem }
-          >
-            Add item
-          </button>
-        </fieldset>
+          index = {
+            index
+          }
+          value = {
+            item.name
+          }
+          onDelete = {
+            this.handleRemoveItem(index)
+          }
+          placeholder = "Name"
+          errors = {
+            errors.items && errors.items[index] &&
+            errors.items[index].name
+          }
+          />
+        )
+      } <
+      button type = "button"
+      className = "btn btn-secondary btn-sm"
+      onClick = {
+        this.handleAddItem
+      } >
+      Add item <
+      /button> <
+      /fieldset>
 
-        <RequestButton
-          className="btn btn-primary"
-          request={ this.state.isSubmit }
-        >
-          Submit
-        </RequestButton>
-      </form>
+      <
+      RequestButton className = "btn btn-primary"
+      request = {
+        this.state.isSubmit
+      } >
+      Submit <
+      /RequestButton> <
+      /form>
     );
   }
 }
 
 export default connect(
-  null,
-  { createPath, addFlashMessage }
+  null, {
+    createPath,
+    addFlashMessage
+  }
 )(withHandlers(NewPathForm));
